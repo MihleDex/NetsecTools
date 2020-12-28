ec71c5597414d5392b2fca71c99cf3f4c043b4e8
Mihle Tshoko <52197701+MihleDex@users.noreply.github.com>
2020-12-28 17:57:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from kivymd.theming import ThemeManager
 import subprocess
 import requests
 from kivy.core.window import Window
-import pprint
-
+from queue import Queue
+import threading
 Window.softinput_mode = "below_target"
 
 KV = '''
+#:import Clock kivy.clock.Clock
 ScreenManager:
     MenuScreen:
+
 
 <MenuScreen>:
     name:"menu"
@@ -36,6 +38,7 @@ ScreenManager:
 	            	border: 10,10,10,10
 	            	background_color:1,1,1,0
 	            	foreground_color:1,1,1,1
+					readonly:True
 
             	FloatLayout:
 	            	MDTextField:
@@ -47,20 +50,21 @@ ScreenManager:
     					mode:"fill"
     					line_color_normal: 0,0,0,1
     					icon_left: 'earth'
-    				MDFloatingActionButton:
-    					icon: "play"
-    					elevation_normal: 8
-    					pos_hint: {"right":.98, "y":.1}
-    					on_release:
-    						displaybox.text = ""
-    						displaybox.text="$ "+app.menu.ping(root.ids.textbox.text)
-    				MDFloatingActionButton:
-    					icon: "delete-forever"
-    					elevation_normal: 8
-    					pos_hint: {"x":0.01, "y":.1}
-    					on_release:
-    						displaybox.text = "$"
-    						textbox.text= ""
+					FloatLayout:
+						MDFloatingActionButton:
+							icon: "play"
+							elevation_normal: 8
+							pos_hint: {"x":0.02, "y":.1}
+							on_release:
+								displaybox.text = ""
+								displaybox.text="$ "+app.menu.ping(root.ids.textbox.text)
+						MDFloatingActionButton:
+							icon: "delete-forever"
+							elevation_normal: 8
+							pos_hint: {"right":.98, "y":.1}
+							on_release:
+								displaybox.text = "$"
+								textbox.text= ""
 
         MDBottomNavigationItem
             name: 'screen 2'
@@ -73,10 +77,11 @@ ScreenManager:
             		title: "Host Checker"
             		specific_text_color: 1,1,1,1
 	            ScrollView:
+					id: scrlv
 		            TextInput:
 		            	id:displaybox2
 		            	text: "$ "
-		            	size_hint_y:1
+		            	size_hint_y:None
 		            	disabled_foreground_color: 1,1,1,1
 		            	border: 10,10,10,10
 		            	background_color:1,1,1,0
@@ -84,10 +89,15 @@ ScreenManager:
 		            	multiline:True
 		            	use_bubble:True
 		            	foreground_color:1,1,1,1
+						cursor:0,0
+						readonly:True
+						on_touch_down: Clock.schedule_once(lambda dt: self.select_all())
+						height: max(self.minimum_height, scrlv.height)
 
 	            FloatLayout:
 	            	MDTextField:
 	            		id:textbox2
+						text: textbox.text
     					hint_text: "Enter Url here"
     					helper_text: "eg.. http://www.URL.com"
     					helper_text_mode: "persistent"
@@ -102,20 +112,22 @@ ScreenManager:
 					        text: 'GET'
 					        icon: 'check-network'
 					        on_release:
-					        	displaybox2.text= "get response:\\n"+ app.menu.get(textbox2.text)
-
-
+					        	displaybox2.text= "Get response:\\n"+ app.menu.get(textbox2.text)
+								displaybox2.cursor = 0,0
+								
 					    MDRectangleFlatIconButton:
 					        text: 'POST'
 					        icon: 'cloud-upload'
 					        on_release:
-					        	displaybox2.text= "post response: "+app.menu.get(textbox2.text)
+					        	displaybox2.text= "Post response:\\n"+app.menu.post(textbox2.text)
+								displaybox2.cursor = 0,0
 					        
 					    MDRectangleFlatIconButton:
 					        text: 'PUT'
 					        icon: 'file-upload'
 					        on_release:
-					        	displaybox2.text="put response: "+app.menu.get(textbox2.text)
+					        	displaybox2.text="Put response:\\n"+app.menu.get(textbox2.text)
+								displaybox2.cursor = 0,0
 					    
 					BoxLayout:
 						orientation:"horizontal" 
@@ -124,23 +136,26 @@ ScreenManager:
 					        text: 'HEAD'
 					        icon: 'format-header-pound'
 					        on_release:
-					        	displaybox2.text="head response: "+app.menu.get(textbox2.text)
+					        	displaybox2.text="Head response:\\n"+app.menu.get(textbox2.text)
+								displaybox2.cursor = 0,0
 					        
 					    MDRectangleFlatIconButton:
 					        text: 'DELETE'
 					        icon: 'delete'
 					        on_release:
-					        	displaybox2.text="delete response: "+app.menu.get(textbox2.text)
+					        	displaybox2.text="Delete response:\\n"+app.menu.get(textbox2.text)
+								displaybox2.cursor = 0,0
 
 					    MDRectangleFlatIconButton:
 					        text: 'OPTIONS'
 					        icon: 'cloud-question'
 					        on_release:
-					        	displaybox2.text="options response: "+app.menu.get(textbox2.text)
+					        	displaybox2.text="Options response:\\n"+app.menu.get(textbox2.text)
+								displaybox2.cursor = 0,0
 
-				    MDRectangleFlatIconButton:
+				    MDFloatingActionButton:
 				        text: 'CLEAR'
-				        icon: 'cancel'
+				        icon: 'delete-forever'
 				        color: 1,.1,.1,1
 				        pos_hint: {"right":1,"y":0}
 				        on_release:
@@ -149,27 +164,54 @@ ScreenManager:
 
         MDBottomNavigationItem:
             name: 'screen 3'
-            text: 'About'
+            text: 'Port Scanner'
             icon: 'information'
 
             BoxLayout:
             	orientation:"vertical"
             	MDToolbar:
-            		title: "About"
+            		title: "Port Scanner"
             		specific_text_color: 1,1,1,1
 
 	            TextInput:
-	            	text: """Fuck off..."""
+					id:displaybox3
+	            	text: """Coming Soon..."""
 	            	size_hint_y:None
 	            	height:100
 	            	disabled_foreground_color: 1,1,1,1
 	            	border: 10,10,10,10
 	            	background_color:1,1,1,0
 	            	foreground_color:1,1,1,1
+					readonly:True
+
 	            FloatLayout:
-
+					MDTextField:
+	            		id:textbox3
+						text: textbox.text
+    					hint_text: "Enter Url here"
+    					helper_text: "eg.. http://www.URL.com"
+    					helper_text_mode: "persistent"
+    					multiline: False
+    					mode:"fill"
+    					line_color_normal: 0,0,0,1
+					FloatLayout:
+						MDFloatingActionButton:
+							icon: "play"
+							color:0,1,0,.5
+							elevation_normal: 8
+							pos_hint: {"right":.98, "y":.1}
+							on_release:
+								displaybox.text = ""
+								displaybox.text="$ "+app.menu.scan(root.ids.textbox.text)
+						MDFloatingActionButton:
+							icon: "delete-forever"
+							elevation_normal: 8
+							pos_hint: {"x":.02, "y":.1}
+							on_release:
+								displaybox3.text = "$"
+								textbox3.text= ""
+					
 '''
-
 class MenuScreen(Screen):
 	def ping(self,host):
 		host = host.replace('https//:','')
@@ -182,8 +224,22 @@ class MenuScreen(Screen):
 			return "No Packets recieved host is down or incorrect url."
 
 	def get(self,host):
+		resp = 0
 		try:
 			r = requests.get("https://"+host)
+			str_r = r.headers
+			s_out =""
+			resp = r.status_code
+			for key,value in str_r.items():
+				s_out = s_out +str(key)+": "+str(value)+"\n"
+		except requests.exceptions.RequestException as e:
+			s_out = str(e)
+			return s_out
+		return "status code:"+str(resp)+"\n"+s_out
+
+	def post(self,host):
+		try:
+			r = requests.post("https://"+host)
 			str_r = r.headers
 			s_out =""
 			resp = 0
@@ -192,52 +248,62 @@ class MenuScreen(Screen):
 				s_out = s_out +str(key)+": "+str(value)+"\n"
 		except requests.exceptions.RequestException as e:
 			s_out = str(e)
-		return s_out+"status code:"+str(resp)
-
-	def post(self,host):
-		try:
-			r = requests.post("https://"+host)
-			str_r = str(r.status_code)
-		except requests.exceptions.RequestException as e:
-			str_r = str(e)
-		return str_r
-
-	def post(self,host):
+			return s_out
+		return "status code:"+str(resp)+"\n"+s_out
+	def put(self,host):
 		try:
 			r = requests.put("https://"+host)
-			str_r = str(r.status_code)
+			str_r = r.headers
+			s_out =""
+			resp = 0
+			resp = r.status_code
+			for key,value in str_r.items():
+				s_out = s_out +str(key)+": "+str(value)+"\n"
 		except requests.exceptions.RequestException as e:
-			str_r = str(e)
-		return str_r
+			s_out = str(e)
+			return s_out
+		return "status code:"+str(resp)+"\n"+s_out
 
 	def head(self,host):
 		try:
 			r = requests.head("https://"+host)
-			str_r = str(r.status_code)
+			str_r = r.headers
+			s_out =""
+			resp = 0
+			resp = r.status_code
+			for key,value in str_r.items():
+				s_out = s_out +str(key)+": "+str(value)+"\n"
 		except requests.exceptions.RequestException as e:
-			str_r = str(e)
-		return str_r
+			s_out = str(e)
+			return s_out
+		return "status code:"+str(resp)+"\n"+s_out
 
 	def delete(self,host):
 		try:
 			r = requests.delete("https://"+host)
-			str_r = str(r.status_code)
+			str_r = r.headers
+			s_out =""
+			resp = 0
+			resp = r.status_code
+			for key,value in str_r.items():
+				s_out = s_out +str(key)+": "+str(value)+"\n"
 		except requests.exceptions.RequestException as e:
-			str_r = str(e)
-		return str_r
-
+			s_out = str(e)
+			return s_out
+		return "status code:"+str(resp)+"\n"+s_out
 	def options(self,host):
 		try:
 			r = requests.options("https://"+host)
-			str_r = str(r.status_code)
+			str_r = r.headers
+			s_out =""
+			resp = 0
+			resp = r.status_code
+			for key,value in str_r.items():
+				s_out = s_out +str(key)+": "+str(value)+"\n"
 		except requests.exceptions.RequestException as e:
-			str_r = str(e)
-		return str_r
-
-#sm = ScreenManager()
-#sm.add_widget(MenuScreen(name="menu"))
-
-
+			s_out = str(e)
+			return s_out
+		return "status code:"+str(resp)+"\n"+s_out
 
 class MainApp(MDApp):
     def build(self):
